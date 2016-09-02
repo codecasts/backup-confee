9e2d33fc9ad2bce17afdcd45687bb7a648eed7b0
Diego Hernandes <diego@hernandev.com>
2016-09-01 22:22:27 -0300

    finish login with password for initial token

--- app/Units/Auth/Http/Controllers/LoginController.php
+++ app/Units/Auth/Http/Controllers/LoginController.php
@@ -3,67 +3,47 @@
 namespace Confee\Units\Auth\Http\Controllers;
 
 use Codecasts\Support\Http\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Confee\Units\Auth\Http\Requests\LoginRequest;
+use Illuminate\Contracts\Auth\Guard;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-
-
     /**
-     * Create a new controller instance.
+     * @var Guard
      */
-    public function __construct()
+    protected $guard;
+
+    public function __construct(Guard $guard)
     {
-        $this->middleware('guest');
+        $this->guard = $guard;
     }
 
-    /**
-     * Handle a login request to the application.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        $this->validateLogin($request);
-
-        // If the class is using the ThrottlesLogins trait, we can automatically throttle
-        // the login attempts for this application. We'll key this by the username and
-        // the IP address of the client making these requests into this application.
-        if ($lockedOut = $this->hasTooManyLoginAttempts($request)) {
-            $this->fireLockoutEvent($request);
-
-            return $this->sendLockoutResponse($request);
+        if ($this->guard->attempt($request->all())) {
+           return $this->sendLoginSuccessResponse();
         }
 
-        $credentials = $this->credentials($request);
-
-        if ($this->guard()->attempt($credentials, $request->has('remember'))) {
-            return $this->sendLoginResponse($request);
-        }
-
-        // If the login attempt was unsuccessful we will increment the number of attempts
-        // to login and redirect the user back to the login form. Of course, when this
-        // user surpasses their maximum number of attempts they will get locked out.
-        if (! $lockedOut) {
-            $this->incrementLoginAttempts($request);
-        }
-
-        return $this->sendFailedLoginResponse($request);
+        return $this->sendLoginFailedResponse();
     }
 
+    protected function sendLoginFailedResponse()
+    {
+        return [
+            'failed' => 'Invalid Credentials.'
+        ];
+    }
 
+    protected function sendLoginSuccessResponse()
+    {
+        return [
+            'token' => $this->generateToken(),
+        ];
+    }
+
+    protected function generateToken()
+    {
+        return JWTAuth::fromUser($this->guard->user());
+    }
 }
